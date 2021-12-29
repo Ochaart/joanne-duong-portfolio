d256ffb61f8b4d67baf69eac956b28d8c2916ea1
Ochaart <ochaart@users.noreply.github.com>
2021-12-29 13:15:33 -0800

    updates

--- components/Project.jsx
+++ components/Project.jsx
@@ -12,7 +12,6 @@ const Project = ({ title, objectPosition, description, maxWidth, image, href, al
         layout="fill"
         objectFit="cover"
         objectPosition={objectPosition}
-        priority
       />
     </div>
     <h3 className=" text-sm md:text2xl mt-2">{title}</h3>
